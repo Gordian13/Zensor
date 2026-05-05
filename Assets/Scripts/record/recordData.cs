a4f2c4e23d560c9a3f45ffd33de1edfccf54209e
tonne96 <antoneckey@pm.me>
2026-05-05 17:09:36 +0200

    edit recordData.cs

--- Assets/Scripts/record/recordData.cs
+++ Assets/Scripts/record/recordData.cs
@@ -1,11 +1,26 @@
 using UnityEngine;
 
+
+public enum RecordFormat
+{
+    Vinyl,
+    Cassette
+}
+
+public enum Speed
+{
+    Fast,
+    Slow
+}
+
 [CreateAssetMenu(fileName = "recordData", menuName = "Zensor/recordData")]
 public class RecordData : ScriptableObject
 {
     public string title;
     public string author;
     public string description;
-    public Sprite sprite;
+    public Sprite sprite; // album cover in jpeg
     public AudioClip audioClip;
+    public RecordFormat format;
+    public Speed speed; // 33 or 45, using pitch in unity
 }
