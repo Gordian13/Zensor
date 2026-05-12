ff719f2d3396143be812d2896ed086c986e9c12d
Nils <nilsscharein@yahoo.de>
2026-05-12 20:54:33 +0200

    feat: finalize ColorReveal logic and add documentation

--- Assets/Scripts/Interaction/util/ColorReveal/IColorRevealable.cs
+++ Assets/Scripts/Interaction/util/ColorReveal/IColorRevealable.cs
@@ -1,7 +1,10 @@
-﻿namespace DefaultNamespace.GrayScale
+﻿namespace Interaction.util.ColorReveal
 {
     public interface IColorRevealable
     {
+        /**
+         * Toggles the Color of the Object that has this Interface
+         */
         void ToggleColor();
     }
 }
