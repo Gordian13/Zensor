03debf7057003b3b00937a89c9aa3f7e86c137e6
Nils <nilsscharein@yahoo.de>
2026-06-14 20:58:19 +0200

    feat: add right click script

---
+++ Assets/Scripts/Core/vinyl/rightClickTurnObject.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Core.vinyl
+{
+    public class RightClickTurnObject : MonoBehaviour
+    {
+        private Vector3 _startRotationPos;
+        
+        #region Input Actions
+
+        [SerializeField] private InputActionAsset _actions;
+
+        public InputActionAsset actions
+        {
+            get => _actions;
+            set => _actions = value;
+        }
+
+        protected InputAction rightClickPressedInputAction { get; set; }
+
+        protected InputAction mouseLookInputAction { get; set; }
+
+        #endregion
+
+        #region Variables
+
+        private bool _rotateAllowed;
+
+        private Camera _camera;
+
+        [SerializeField] private float _speed;
+
+        [SerializeField] private bool _inverted;
+
+        #endregion
+
+        private void Awake()
+        {
+            _startRotationPos = this.transform.localRotation.eulerAngles;
+            InitializeInputSystem();
+        }
+
+        private void Start()
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+
+            _camera = Camera.main;
+        }
+
+        private void InitializeInputSystem()
+        {
+            rightClickPressedInputAction = actions.FindAction("Right Click");
+            if (rightClickPressedInputAction != null)
+            {
+                rightClickPressedInputAction.started += OnRightClickPressed;
+                rightClickPressedInputAction.performed += OnRightClickPressed;
+                rightClickPressedInputAction.canceled += OnRightClickPressed;
+            }
+
+            mouseLookInputAction = actions.FindAction("Mouse Look");
+
+            actions.Enable();
+        }
+
+        protected virtual void OnRightClickPressed(InputAction.CallbackContext context)
+        {
+            if (context.started || context.performed)
+            {
+                _rotateAllowed = true;
+            }
+            if (context.canceled)
+            {
+                _rotateAllowed = false;
+                ReturnToRotation(_startRotationPos);
+            }
+        }
+
+        public void ReturnToRotation(Vector3 rotation)
+        {
+            this.transform.eulerAngles = rotation;
+        }
+
+        protected virtual Vector2 GetMouseLookInput()
+        {
+            if (mouseLookInputAction != null)
+                return mouseLookInputAction.ReadValue<Vector2>();
+
+            return Vector2.zero;
+        }
+
+        private void Update()
+        {
+            if (!_rotateAllowed)
+                return;
+
+            Vector2 MouseDelta = GetMouseLookInput();
+
+            MouseDelta *= _speed * Time.deltaTime;
+
+            transform.Rotate(Vector3.up * (_inverted ? 1 : -1), MouseDelta.x, Space.World);
+            transform.Rotate(Vector3.right * (_inverted ? -1 : 1), MouseDelta.y, Space.World);
+        }
+    }
+}
